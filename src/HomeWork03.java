03b639fa7bd387cc9166f388b7e950a6ea819dc5
FedirTaran <flondon0828@gmail.com>
2023-01-03 10:44:33 +0100

    03.01 commit

---
+++ src/HomeWork03.java
@@ -0,0 +1,43 @@
+
+public class HomeWork03 {
+
+    public static void main(String[] args) {
+
+        byte byteValue = 12;
+
+        short shortValue = 13;
+
+        int intValue = 123;
+
+        long longValue = 10L;
+
+        long longValue01 = 10L;
+
+        float floatValue = 12.5F;
+
+        double doubleValue = 12.6;
+
+        char charValue = 15;
+
+        char charValue01 = 125;
+
+        boolean booleanValue = true;
+
+        int testIntValue01 = 129;
+
+        byte byteValue01 = (byte)testIntValue01;
+
+        Integer integer = 2;
+        Integer Integer01 = Integer.valueOf("1243");
+        Double aDouble = 17.6;
+        String text = "text";
+
+
+        System.out.println(testIntValue01);
+        System.out.println(charValue);
+        System.out.println(floatValue);
+        System.out.println(booleanValue);
+    }
+
+}
+
